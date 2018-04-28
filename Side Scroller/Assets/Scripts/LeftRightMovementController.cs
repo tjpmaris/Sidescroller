ecf0ad87da6ae5368010d492b436393edacdcca3
lunarnublet <lunarnublet@gmail.com>
2018-04-28 11:12:03 -0600

    First commit.

---
+++ Side Scroller/Assets/Scripts/LeftRightMovementController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightMovementController : MonoBehaviour {
+    private Transform transform;
+	// Use this for initialization
+	void Start () {
+        transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMultiplier = Input.GetAxis("Horizontal");
+        float horizontalAddition = horizontalMultiplier * PlayerModel.speed;
+
+        transform.position = new Vector3(transform.position.x + horizontalAddition, transform.position.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.LogWarning("TriggerEnter");
+        Debug.Log(GetComponent<BoxCollider2D>().gameObject.name);
+        Debug.Log(other.gameObject.name);
+        //canWalk = false;
+
+        //other.transform
+
+    }
+
+    private void LateUpdate()
+    {
+        //if (physics.velocity.y >= -.001 && physics.velocity.y <= .001)
+        //{
+        //    PlayerModel.isGrounded = true;
+        //}
+        //else
+        //{
+        //    PlayerModel.isGrounded = false;
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    if (PlayerModel.isGrounded)
+        //    {
+        //        Vector2 movement = new Vector2(0, 1);
+        //        physics.AddForce(movement * PlayerModel.jumpSpeed, ForceMode2D.Impulse);
+        //    }
+        //}
+    }
+}
---
+++ Side Scroller/Assets/Scripts/PlayerModelSetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerModelSetter : MonoBehaviour {
+
+    public float speed;
+    public float jumpSpeed;
+    // Use this for initialization
+    void Start () {
+        PlayerModel.jumpSpeed = jumpSpeed;
+        PlayerModel.speed = speed;
+        PlayerModel.isGrounded = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
